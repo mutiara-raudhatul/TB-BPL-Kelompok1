f7d3ed422b8e157727c93ad3136db0af3d8d5891
mutiara-raudhatul <1911521011_mutiara@student.unand.ac.id>
2020-12-05 11:21:25 +0700

    commit

---
+++ db_kasir.sql
@@ -0,0 +1,129 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 05, 2020 at 05:06 AM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.3.15
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_kasir`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `barang`
+--
+
+CREATE TABLE `barang` (
+  `sku` varchar(50) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `stok` int(50) NOT NULL,
+  `harga_beli` int(50) NOT NULL,
+  `harga_jual` int(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `noresi` varchar(30) NOT NULL,
+  `tanggal` date NOT NULL,
+  `username` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaksi_detail`
+--
+
+CREATE TABLE `transaksi_detail` (
+  `id` int(30) NOT NULL,
+  `sku` varchar(50) NOT NULL,
+  `noresi` varchar(30) NOT NULL,
+  `jumlah` int(50) NOT NULL,
+  `harga` int(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `username` varchar(30) NOT NULL,
+  `login_terakhir` date NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `password` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `barang`
+--
+ALTER TABLE `barang`
+  ADD PRIMARY KEY (`sku`);
+
+--
+-- Indexes for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`noresi`),
+  ADD KEY `username` (`username`);
+
+--
+-- Indexes for table `transaksi_detail`
+--
+ALTER TABLE `transaksi_detail`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `sku` (`sku`),
+  ADD KEY `noresi` (`noresi`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`username`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD CONSTRAINT `transaksi_ibfk_1` FOREIGN KEY (`username`) REFERENCES `user` (`username`);
+
+--
+-- Constraints for table `transaksi_detail`
+--
+ALTER TABLE `transaksi_detail`
+  ADD CONSTRAINT `transaksi_detail_ibfk_1` FOREIGN KEY (`sku`) REFERENCES `barang` (`sku`),
+  ADD CONSTRAINT `transaksi_detail_ibfk_2` FOREIGN KEY (`noresi`) REFERENCES `transaksi` (`noresi`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- src/id/ac/unand/fti/si/Program.java
+++ src/id/ac/unand/fti/si/Program.java
@@ -1,9 +1,29 @@
 package id.ac.unand.fti.si;
 
-public class Program {
+import java.sql.*;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class Program {
+	// Menyiapkan paramter JDBC untuk koneksi ke database
+	static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+	static final String DB_URL = "jdbc:mysql://localhost/db_kasir?serverTimezone=Asia/Jakarta";
+	static final String USER = "root";
+	static final String PASS = "";
+	
+	// Menyiapkan objek yang diperlukan untuk mengelola database
+	static Connection conn;
+	
+	public static void main(String[] args) throws Exception {		
+
+		try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+     	    conn = DriverManager.getConnection(DB_URL, USER, PASS);
+            
+            
+            conn.close();
+        } catch (ClassNotFoundException e) {
+            System.out.println("Terjadi Kesalahan : Driver tidak ditemukan");
+        }
+    
 		System.out.print("Program");
 	}
 
